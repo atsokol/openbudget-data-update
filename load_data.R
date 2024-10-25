33fa5fab5baee7ae2b310cf9af9e5ed85742206a
Andrii Tsokol <atsokol@gmail.com>
2024-10-25 23:13:58 +0200

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -22,7 +22,7 @@ write_csv(data[[1]] |> left_join(city_codes, join_by(COD_BUDGET == value)) |> re
 write_csv(data[[2]] |> left_join(city_codes, join_by(COD_BUDGET == value)) |> rename(CITY = city),
           "data/expenses.csv")
 write_csv(data[[3]] |>
-      group_by(REP_PERIOD, FUND_TYP, COD_BUDGET, COD_CONS_MB_FK, COD_CONS_MB_FK_NAME, CITY) |>
+      group_by(REP_PERIOD, FUND_TYP, COD_BUDGET, COD_CONS_MB_FK, COD_CONS_MB_FK_NAME) |>
       summarise_if(is.numeric, sum, na.rm = TRUE) |>
       left_join(city_codes, join_by(COD_BUDGET == value)) |>
       rename(CITY = city),
